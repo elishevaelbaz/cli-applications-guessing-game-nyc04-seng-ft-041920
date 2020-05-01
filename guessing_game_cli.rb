d6016fc8e6347b618eef3fe913c948b8ad0e9b2d
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-05-01 14:46:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   
   if guess.to_i == rand_int
     puts "You guessed the correct number!"
-  elsif guess == "e"
+  elsif guess == "exit"
     puts "Goodbye!"
   else 
     puts "Sorry! The computer guessed #{rand_int}."
